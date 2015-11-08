389d037d7f1002d4189e8bfa1810429d313e444c
emanulzone <martin@emanuelzone.se>
2015-11-08 20:09:41 +0100

    Update autoloader.php

--- autoloader.php
+++ autoloader.php
@@ -15,7 +15,7 @@ spl_autoload_register(
     function ($class) {
         // project-specific namespace prefix
         //$prefix = 'Foo\\Bar\\';
-        $prefix = 'ezon\\';
+        $prefix = 'Ezon\\';
         // base directory for the namespace prefix
         $base_dir = __DIR__ . '/src/';
         // does the class use the namespace prefix?
